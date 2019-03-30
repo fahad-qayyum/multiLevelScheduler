94f887775b64675092efe10862b2272b5c064afd
Fahad Qayyum <fahadq@my.yorku.ca>
2019-03-30 17:42:31 -0400

    Update q2.c

--- q2.c
+++ q2.c
@@ -15,13 +15,13 @@
 #include <pthread.h>
 #include "Queue.h"
 
-void *longScheduler(void *args);  // thread representing long term scheduler
-void * shortScheduler(void *args);  // thread representing short term scheduler
-Queue *newQueue, *readyQueue;   // creating 2 queues 
-bool button;			                //true for long term scheduler and false for short term
-pthread_t longTid, shortTid;  // for long and short threads
-pthread_mutex_t lock;					  // for locking and unlocking
-pthread_cond_t check;            // for checkition checking
+void *longScheduler(void *args);  		 // thread representing long term scheduler
+void * shortScheduler(void *args); 		 // thread representing short term scheduler
+Queue *newQueue, *readyQueue;   		 // creating 2 queues 
+bool button;			                 // true for long term scheduler and false for short term
+pthread_t longTid, shortTid;  			 // for long and short threads
+pthread_mutex_t lock;			         // for locking and unlocking
+pthread_cond_t check;                            // for checkition checking
 
 
 int main()
@@ -29,7 +29,7 @@ int main()
 	int counter;
 	button = true;
 	srand(time(NULL)); 
-  newQueue = makeQueue();
+  	newQueue = makeQueue();
 	readyQueue = makeQueue();
 
   //-------------Populating the NEW queue-------------------
@@ -43,17 +43,17 @@ int main()
  
  //--------------Thread handling and manipulation------------
  
-	pthread_mutex_init(&lock, NULL);  // defining mutex lock
-	pthread_cond_init(&check, NULL);  // defining cond check
-  pthread_mutex_lock(&lock);  // locking the thread 
-	pthread_create(&longTid, NULL, longScheduler, NULL);  // creating long scheduler thread
-	pthread_create(&shortTid, NULL, shortScheduler, NULL);  // creating short scheduler thread
-	pthread_cond_signal(&check);  // checking signal
-	pthread_mutex_unlock(&lock);  // mutex unlocking
-	pthread_join(longTid, NULL);  // join for long thread    
-	pthread_join(shortTid, NULL);  // join for short thread   
-	pthread_mutex_destroy(&lock);  // destroying the lock
-	pthread_cond_destroy(&check);  // destroying the check 
+	pthread_mutex_init(&lock, NULL);  				// defining mutex lock
+	pthread_cond_init(&check, NULL);				// defining cond check
+  	pthread_mutex_lock(&lock);  					// locking the thread 
+	pthread_create(&longTid, NULL, longScheduler, NULL); 		// creating long scheduler thread
+	pthread_create(&shortTid, NULL, shortScheduler, NULL); 	 	// creating short scheduler thread
+	pthread_cond_signal(&check);  					// checking signal
+	pthread_mutex_unlock(&lock);  					// mutex unlocking
+	pthread_join(longTid, NULL);  					// join for long thread    
+	pthread_join(shortTid, NULL); 	 				// join for short thread   
+	pthread_mutex_destroy(&lock);  					// destroying the lock
+	pthread_cond_destroy(&check);  					// destroying the check 
      
 	return 0;
 }
@@ -63,11 +63,11 @@ int main()
 void *longScheduler(void *args)
 {  
 	printf("Long scheduler STARTED!\n");
-	while(sizeOf(newQueue) > 0 || sizeOf(readyQueue) > 0){  // do until something in the queue
+	while(sizeOf(newQueue) > 0 || sizeOf(readyQueue) > 0){  	// do until something in the queue
   pthread_mutex_lock(&lock);	  // getting locks
 		if(!button){
-			printf("Long scheduler PAUSED!\n");			
-			pthread_cond_wait(&check, &lock);  // conditional wait
+			printf("Long scheduler PAUSED!\n");				
+			pthread_cond_wait(&check, &lock); 	 	// conditional wait
 			printf("Long scheduler in ACTION!\n");
 		}
   button = false;
@@ -94,19 +94,27 @@ void * shortScheduler(void *args)
   int counter;
 	printf("Short scheduler STARTED!\n");
 	while(true){
-		pthread_mutex_lock(&lock);  // locking mutex
+		
+		// locking mutex
+		pthread_mutex_lock(&lock);  
 		if(button){
-			pthread_cond_wait(&check, &lock);  // conditional wait
+			
+			// conditional wait
+			pthread_cond_wait(&check, &lock); 
 			printf("Short scheduler in Action!\n");	
 		}
 		button = true;
-		for(counter = 0; sizeOf(readyQueue) > 0 && counter < 5; counter++){  // enqueing 5 processes in READY
+		for(counter = 0; sizeOf(readyQueue) > 0 && counter < 5; counter++){ 
+			
+			 // enqueing 5 processes in READY
 			Task *Task = dequeue(readyQueue);
-      printf("---------------------------------------\n");
-      printf("Currently Executing:- \n");
+    			printf("---------------------------------------\n");
+     			printf("Currently Executing:- \n");
 			printf("Process ID : %d with TIME : %d\n", Task->pid, Task->time);
-      printf("---------------------------------------\n");
-			Task->time -= 2;  // decreasing time for every process by 2
+      			printf("---------------------------------------\n");
+			
+			// decreasing time for every process by 2
+			Task->time -= 2;  
 			if(Task->time > 0){
 				enqueue(readyQueue, Task);  // enqueing back again
 				printf("Process ID: %d moved back!\n", Task->pid);	
@@ -114,10 +122,15 @@ void * shortScheduler(void *args)
 				printf("Process ID : %d TERMINATED!\n", Task->pid);
 			}	
 		}
-		pthread_cond_signal(&check);  // signalizing the check
-		pthread_mutex_unlock(&lock);  // releasing the locks
+		// signalizing the check
+		pthread_cond_signal(&check);  
+		
+		// releasing the locks
+		pthread_mutex_unlock(&lock);  
    if(sizeOf(readyQueue) == 0){
-			printf("Short scheduler STOPPED!\n");  // stop if the queue gets empty
+	   
+	  		 // stop if the queue gets empty
+			printf("Short scheduler STOPPED!\n");  
 			break;	
 		}
 	}	
